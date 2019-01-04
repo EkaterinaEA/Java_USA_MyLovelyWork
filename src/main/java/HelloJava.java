9cef4364832b6938ff5db9f25d7ccdaf39679102
Екатерина <krasnovaEA95@yandex.ru>
2019-01-03 23:24:52 -0500

    add class HelloJava

---
+++ src/main/java/HelloJava.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Екатерина on 03.01.2019.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, second course!");
+    }
+}
