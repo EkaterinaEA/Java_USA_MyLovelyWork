f502459e17b05b9cf3292dffae3bc45010bc78e8
Екатерина <krasnovaEA95@yandex.ru>
2019-01-04 00:14:06 -0500

    Changed sout String

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -3,6 +3,6 @@
  */
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello, second course! I love work with Java. Java is my love!");
+        System.out.println("Hello, second course! I love work with Java. Java is my love! ");
     }
 }
